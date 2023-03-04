790cdaa8102ed580fecc57b02f613acb2fe52b56
unknown <ravitejareddy.seemala@gmail.com>
2023-03-05 02:41:53 +0530

    removed cred

--- EC2 project/scraper.py
+++ EC2 project/scraper.py
@@ -11,22 +11,22 @@ def get_time():
     return(time_str)
 
 num = random.randint(5, 40)
-Access_key='AKIA4ZSUVQ5PNEUI25VH'
-Secret_key='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'
 s='start'
 print("start")
 for i in range(num):
     print(get_time())
     s += ','+str(get_time())
-    time.sleep(60)
+    time.sleep(2)
 print('end')
 s += ','+'end'
-client = boto3.client(
-    's3',
-    aws_access_key_id=Access_key,
-    aws_secret_access_key=Secret_key)
-s3 = client
+s3 = boto3.client('s3')
 bucket_name = 'bucketmaharshi'
 object_key = str(num)+' min'
 response = s3.put_object(Bucket=bucket_name, Key=object_key, Body=s)
 print(s)
+region = 'ap-south-1'
+instances = ['i-0eb790bc1a0c0a808']
+#ec2 = boto3.client('ec2', region_name=region)
+ec2 = boto3.client('ec2',region_name=region)
+ec2.stop_instances(InstanceIds=instances)
+print('you should not be able to see this')
